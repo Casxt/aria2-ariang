d4bb266fa9fbd62c72359af570def525ec9e3065
Forer <774714620@qq.com>
2019-01-29 15:39:20 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 if [ ! -f /aria2/conf/aria2.conf ]; then
-	sudo cp /aria2/conf-temp/aria2.conf /aria2/conf/aria2.conf
+	cp /aria2/conf-temp/aria2.conf /aria2/conf/aria2.conf
 	if [ $SECRET ]; then
 		echo "rpc-secret=${SECRET}" >> /aria2/conf/aria2.conf
 	fi
 fi
 if [ ! -f /aria2/conf/on-complete.sh ]; then
-	sudo cp /aria2/conf-temp/on-complete.sh /aria2/conf/on-complete.sh
+	cp /aria2/conf-temp/on-complete.sh /aria2/conf/on-complete.sh
 fi
 
 chmod +x /aria2/conf/on-complete.sh
 touch /aria2/conf/aria2.session
 
-sudo darkhttpd /aria-ng --port 80 &
+darkhttpd /aria-ng --port 80 &
 # darkhttpd /aria2/downloads --port 8080 &
-sudo aria2c --conf-path=/aria2/conf/aria2.conf
+aria2c --conf-path=/aria2/conf/aria2.conf
